52bd63f6d1a1eb5a633f86dd55babc38046cc7c0
martin0123478 <martin_agui97@outlook.es>
2024-09-17 18:01:54 -0600

    feat:create form and styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,10 @@
-import { Link, NavLink, useLocation } from "react-router-dom"
+import { useMemo } from "react"
+import { NavLink, useLocation } from "react-router-dom"
 
 export const Header = () => {
-    const location = useLocation()
-    console.log(location)
+    const { pathname } = useLocation()
+    const isHome = useMemo(() => pathname === '/', [pathname])
+    console.log(isHome)
     return (
         <header className='bg-slate-800'>
             <div className='mx-auto container px-5 py-16'>
@@ -17,7 +19,26 @@ export const Header = () => {
                     </nav>
 
                 </div>
+                {isHome && (
+                    <form className="md:w-1/2 2xl:w-1/3 bg-orange-400 my-32 p-10 rounded-lg shadow space-y-6">
+                        <div className="space-y-4">
+                            <label className="block text-white uppercase font-extrabold text-lg"
+                                htmlFor="ingredient">Nombre o Ingredientes</label>
+                            <input type="text" id="ingredient" name="ingrediente" className="p-3 w-full rounded-lg focus:outline-none"
+                                placeholder="Nombre o Ingrediente" />
+                        </div>
+                        <div className="space-y-4">
+                            <label className="block text-white uppercase font-extrabold text-lg"
+                                htmlFor="ingredient">Categoria</label>
+                            <select id="ingredient" name="ingrediente" className="p-3 w-full rounded-lg focus:outline-none"
+                            >
+                                <option>Selecciones</option>
+                            </select>
+                        </div>
+                        <input className="cursor-pointer bg-orange-800 hover:bg-orange-900 text-white font-extrabold w-full p-2 rounded-lg uppercase" type="submit" value='Buscar Recetas' />
+                    </form>
+                )}
             </div>
-        </header>
+        </header >
     )
 }
